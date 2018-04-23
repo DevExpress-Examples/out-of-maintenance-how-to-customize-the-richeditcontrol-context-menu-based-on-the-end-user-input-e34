69fdebc45ac14e242a5e0accb3c80da6f9b4b115
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/MainPage.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using DevExpress.Xpf.Bars;
+using DevExpress.Xpf.RichEdit;
+using DevExpress.Xpf.RichEdit.Menu;
+using DevExpress.XtraRichEdit.API.Native;
+using DevExpress.XtraRichEdit.Utils;
+
+namespace RichEditInputSpecificMenuSL {
+    public partial class MainPage : UserControl {
+        private string keyTextTable = "Table:";
+        private string keyTextView = "View:";
+        private string userCommand = string.Empty;
+        private Document Document { get { return reEditor.Document; } }
+        
+        public MainPage() {
+            InitializeComponent();
+        }
+
+        private void reEditor_Loaded(object sender, RoutedEventArgs e) {
+            reEditor.ApplyTemplate();
+            reEditor.KeyCodeConverter.Focus();
+        }
+
+        private void RichEditControl_ContentChanged(object sender, EventArgs e) {
+            int maxKeyLength = Math.Max(keyTextTable.Length, keyTextView.Length);
+            string text = Document.GetText(Document.CreateRange(Math.Max(0, Document.CaretPosition.ToInt() - maxKeyLength), maxKeyLength));
+
+            if (text.EndsWith(keyTextTable) || text.EndsWith(keyTextView)) {
+                if (text.EndsWith(keyTextTable))
+                    userCommand = keyTextTable;
+                else if (text.EndsWith(keyTextView))
+                    userCommand = keyTextView;
+                else
+                    userCommand = string.Empty;
+                
+                System.Drawing.Rectangle rect = reEditor.GetBoundsFromPosition(Document.CaretPosition);
+                System.Drawing.Rectangle localRect = Units.DocumentsToPixels(rect, reEditor.DpiX, reEditor.DpiY);
+                System.Drawing.Point localPoint = new System.Drawing.Point(localRect.Right, localRect.Bottom);
+
+                GeneralTransform generalTransform = this.TransformToVisual(reEditor);
+                Point point = generalTransform.Transform(new Point(localPoint.X, localPoint.Y));
+                reEditor.ShowMenu(point);
+            }
+        }
+
+        private void reEditor_PopupMenuShowing(object sender, PopupMenuShowingEventArgs e) {
+            if (!string.IsNullOrEmpty(userCommand)) {
+                e.Menu.ItemLinks.Add(new BarItemLinkSeparator());
+
+                if (userCommand == keyTextTable) {
+                    RichEditMenuItem customItem = new RichEditMenuItem();
+                    customItem.Content = "Insert Table";
+                    customItem.Command = RichEditUICommand.InsertTable;
+                    customItem.CommandParameter = reEditor;
+                    e.Menu.ItemLinks.Add(customItem);
+                }
+                else {
+                    RichEditMenuItem customItem1 = new RichEditMenuItem();
+                    customItem1.Content = "View Print Layout";
+                    customItem1.Command = RichEditUICommand.ViewPrintLayout;
+                    customItem1.CommandParameter = reEditor;
+                    e.Menu.ItemLinks.Add(customItem1);
+
+                    RichEditMenuItem customItem2 = new RichEditMenuItem();
+                    customItem2.Content = "View Draft";
+                    customItem2.Command = RichEditUICommand.ViewDraft;
+                    customItem2.CommandParameter = reEditor;
+                    e.Menu.ItemLinks.Add(customItem2);
+
+                    RichEditMenuItem customItem3 = new RichEditMenuItem();
+                    customItem3.Content = "View Simple";
+                    customItem3.Command = RichEditUICommand.ViewSimple;
+                    customItem3.CommandParameter = reEditor;
+                    e.Menu.ItemLinks.Add(customItem3);
+                }
+
+                userCommand = string.Empty;
+            }
+        }
+    }
+
+    public class CustomRichEditControl : RichEditControl {
+        public void ShowMenu(Point point) {
+            base.Focus();
+            base.OnPopupMenu(point);
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,9 @@
+# How to customize the RichEditControl context menu based on the end-user input
+
+
+<p>This example illustrates how to customize the RichEditControl context menu based on the end-user input. The main idea of this functionality implementation is to handle the <a href="http://documentation.devexpress.com/#Silverlight/DevExpressXpfRichEditRichEditControl_ContentChangedtopic"><u>RichEditControl.ContentChanged Event</u></a> and analyze characters displayed nearby the caret. The following <a href="http://documentation.devexpress.com/#Silverlight/clsDevExpressXtraRichEditAPINativeDocumenttopic"><u>Document Interface</u></a> members are used for this purpose:</p><p><a href="http://documentation.devexpress.com/#CoreLibraries/DevExpressXtraRichEditAPINativeSubDocument_GetTexttopic878"><u>SubDocument.GetText Method</u></a><br />
+<a href="http://documentation.devexpress.com/#CoreLibraries/DevExpressXtraRichEditAPINativeDocument_CaretPositiontopic"><u>Document.CaretPosition Property</u></a></p><p>Then the <a href="http://documentation.devexpress.com/#Silverlight/DevExpressXpfRichEditRichEditControl_GetBoundsFromPositiontopic"><u>RichEditControl.GetBoundsFromPosition Method</u></a> is called to obtain a location where the context menu should be invoked. Take special note of the way a <a href="http://documentation.devexpress.com/#Silverlight/clsDevExpressXpfRichEditRichEditControltopic"><u>RichEditControl Class</u></a> descendant is created  with the <strong>ShowMenu</strong> method, which allows you to invoke the menu programmatically. When the menu is invoked, the <a href="http://documentation.devexpress.com/#Silverlight/DevExpressXpfRichEditRichEditControl_PopupMenuShowingtopic"><u>RichEditControl.PopupMenuShowing Event</u></a> is raised, which allows you to customize the menu items.</p>
+
+<br/>
+
+
